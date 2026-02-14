0c197e52b0586e61ede57a7b6bd61be28f725ba8
Dani Alonso <danielalonso308@gmail.com>
2026-02-14 10:08:09 +0100

    Capacitor initial config

---
+++ android/app/src/main/java/com/barikwrapped/barikwrapped/MainActivity.java
@@ -0,0 +1,5 @@
+package com.barikwrapped.barikwrapped;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.barikwrapped.barikwrapped',
+  appName: 'barikwrapped',
+  webDir: 'build'
+};
+
+export default config;
